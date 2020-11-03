fed2c2bec2b896884b220da651c19d569f7e0fdd
robpolisena <robpolisena@gmail.com>
2020-11-03 10:23:02 -0500

    updated Navbar and created button component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,9 @@ import "./Navbar.css";
 function Navbar() {
   const [click, setClick] = useState(false);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
       {/* Logo is centered because of navbar justify content center */}
@@ -14,9 +17,48 @@ function Navbar() {
           <Link to="/" className="navbar-logo">
             <img className="logo fab fa-typo3" src={Logo} alt="Rob Logo" />
           </Link>
-          <div className="menu-icon">
+          <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav=links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav=links" onClick={closeMobileMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/skills"
+                className="nav=links"
+                onClick={closeMobileMenu}
+              >
+                Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/projects"
+                className="nav=links"
+                onClick={closeMobileMenu}
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className="nav=links"
+                onClick={closeMobileMenu}
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
@@ -24,3 +66,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
+//TRVL <i className="fab fa-typo3" />
